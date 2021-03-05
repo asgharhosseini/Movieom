8ca0d08eb53b5e0eabd8d7cef11d754d9a411de0
asghar hosseini <asgharhosseini.v@gmail.com>
2021-03-05 18:25:07 +0330

    getting movie with genreId and showing in view

--- app/src/main/java/ir/vbile/app/movieom/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/ir/vbile/app/movieom/ui/fragment/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.*
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.*
 import androidx.recyclerview.widget.*
+import com.bumptech.glide.*
 import com.google.android.material.snackbar.*
 import dagger.hilt.android.AndroidEntryPoint
 import ir.vbile.app.movieom.*
@@ -17,6 +18,7 @@ import ir.vbile.app.movieom.other.Constance.Companion.TAG
 import ir.vbile.app.movieom.ui.adapter.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.*
+import kotlin.random.*
 
 @AndroidEntryPoint
 class HomeFragment:Fragment(R.layout.fragment_home) {
@@ -25,12 +27,23 @@ class HomeFragment:Fragment(R.layout.fragment_home) {
 
     private lateinit var genresAdapter: GenreAdapter
 
+    @Inject
+     lateinit var moviesTopAdapter: MoviesAdapter
+
+
+    var top = Random.nextInt(1, 21)
+    var center = Random.nextInt(1, 21)
+    var down = Random.nextInt(1, 21)
     private var animationFlag = false
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
         subscribeToObservers()
         setupView()
+        vm.getGenresMovies(top)
 
     }
 
@@ -43,6 +56,13 @@ class HomeFragment:Fragment(R.layout.fragment_home) {
                     )
             findNavController().navigate(action)
         }
+        moviesTopAdapter.setOnItemClickListener {item->
+            val action =
+                    HomeFragmentDirections.actionHomeFragmentToDetailFragment(
+                            item.id
+                    )
+            findNavController().navigate(action)
+        }
     }
 
 
@@ -50,8 +70,18 @@ class HomeFragment:Fragment(R.layout.fragment_home) {
         val resId = R.anim.layout_animation_slide_right
         val animation = AnimationUtils.loadLayoutAnimation(context, resId)
         genresAdapter= GenreAdapter()
+        //moviesTopAdapter= MoviesAdapter()
         rv_genres_fragmentHome.apply {
             adapter = genresAdapter
+            if (!animationFlag) {
+                layoutAnimation = animation
+                animationFlag = true
+            }
+            layoutManager = LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
+        }
+
+        rv_top_fragmentHome.apply {
+            adapter = moviesTopAdapter
             if (!animationFlag) {
                 layoutAnimation = animation
                 animationFlag = true
@@ -67,6 +97,9 @@ class HomeFragment:Fragment(R.layout.fragment_home) {
                     hideProgressBar()
                     result.data?.let {
                         genresAdapter.differ.submitList(it.toList())
+                        txt_titleTop_fragmentHome.text = it[top].name
+                        txt_titleCenter_fragmentHome.text = it[center].name
+                        txt_titleDown_fragmentHome.text = it[down].name
 
                     }
                 }
@@ -80,6 +113,27 @@ class HomeFragment:Fragment(R.layout.fragment_home) {
                 is Resource.Loading -> {
                     showProgressBar()
 
+                }
+            }
+        })
+        vm.genresMovies.observe(viewLifecycleOwner, Observer {
+            result ->
+            when(result) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    result.data?.let {
+                        moviesTopAdapter.differ.submitList(it.data.toList())
+                    }
+                }
+                is Resource.Error -> {
+                    showProgressBar()
+                    result.message?.let { message->
+                        Log.e(TAG,message)
+                        Snackbar.make(requireView(),"Error: $message ${result.code}", Snackbar.LENGTH_LONG).show()
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
                 }
             }
         })
