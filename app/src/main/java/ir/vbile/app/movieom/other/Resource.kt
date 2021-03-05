7e23529c5c9b85ebbafecd7a0b05f0056a731d3e
asghar hosseini <asgharhosseini.v@gmail.com>
2021-03-05 14:36:29 +0330

    Rebuilding Resource.kt class

--- app/src/main/java/ir/vbile/app/movieom/other/Resource.kt
+++ app/src/main/java/ir/vbile/app/movieom/other/Resource.kt
@@ -2,21 +2,16 @@ package ir.vbile.app.movieom.other
 
 import okhttp3.ResponseBody
 
-data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
-    companion object {
-        fun <T> success(data: T?): Resource<T> {
-            return Resource(Status.SUCCESS, data, null)
-        }
-
-        fun <T> error(msg: String, data: T?): Resource<T> {
-            return Resource(Status.ERROR, data, msg)
-        }
-
-        fun <T> loading(data: T?): Resource<T> {
-            return Resource(Status.LOADING, data, null)
-        }
-    }
+sealed class Resource<T>(
+        val data: T? = null,
+        val message: String? = null,
+        val code:Int?=null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null,code: Int?) : Resource<T>(data, message,code)
+    class Loading<T> : Resource<T>()
 }
+
 enum class Status {
     SUCCESS,
     ERROR,
