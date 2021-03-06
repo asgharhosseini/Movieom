063442893e076a5d8668b6771485b6b57d4f2c44
asghar hosseini <asgharhosseini.v@gmail.com>
2021-03-06 21:10:35 +0330

    create  GenreFragment.kt and GenreMoviesAdapter.kt for show view
    Signed-off-by: asghar hosseini <asgharhosseini.v@gmail.com>

--- app/src/main/java/ir/vbile/app/movieom/ui/fragment/genre/GenreFragment.kt
+++ app/src/main/java/ir/vbile/app/movieom/ui/fragment/genre/GenreFragment.kt
@@ -1,15 +1,95 @@
 package ir.vbile.app.movieom.ui.fragment.genre
 
 import android.os.*
+import android.util.*
 import android.view.*
+import android.view.animation.*
 import androidx.fragment.app.*
-import dagger.hilt.android.AndroidEntryPoint
-import ir.vbile.app.movieom.*
-@AndroidEntryPoint
-class GenreFragment:Fragment(R.layout.fragment_genre) {
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
+import androidx.recyclerview.widget.*
+import com.google.android.material.snackbar.*
+import dagger.hilt.android.*
+import ir.vbile.app.movieom.R
+import ir.vbile.app.movieom.other.*
+import ir.vbile.app.movieom.ui.adapter.*
+import kotlinx.android.synthetic.main.fragment_genre.*
+import javax.inject.*
 
+@AndroidEntryPoint
+class GenreFragment : Fragment(R.layout.fragment_genre) {
+    private var animationFlag = false
+    private val vm: GenreViewModel by viewModels()
+
+    private val arg by navArgs<GenreFragmentArgs>()
+
+    @Inject
+    lateinit var genreMoviesAdapter: GenreMoviesAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        vm.getGenresMovies(arg.genreId)
+        subscribeToObservers()
+        setupRecyclerView()
+        setupView()
+    }
+
+
+    fun setupView() {
+        genreMoviesAdapter.setOnItemClickListener { item ->
+            val action =
+                    GenreFragmentDirections.actionGenreFragmentToDetailFragment(
+                            item.id)
+            findNavController().navigate(action)
+        }
+        tv_title_fragmentGenre.text = arg.genreName
+        iv_fragmentGenre_backBottom.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+
+    private fun setupRecyclerView() {
+        val resId = R.anim.layout_animation_fall_down
+        val animation = AnimationUtils.loadLayoutAnimation(context, resId)
+        rv_fragmentGenre.apply {
+            adapter = genreMoviesAdapter
+            if (!animationFlag) {
+                layoutAnimation = animation
+                animationFlag = true
+            }
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        }
+    }
+
+    private fun subscribeToObservers() {
+        vm.genreMovie.observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    result.data?.let {
+                        genreMoviesAdapter.differ.submitList(it.data.toList())
+                    }
+                }
+                is Resource.Error -> {
+                    showProgressBar()
+                    result.message?.let { message ->
+                        Log.e(Constance.TAG, message)
+                        Snackbar.make(requireView(), "Error: $message ${result.code}", Snackbar.LENGTH_LONG).show()
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        genreFragment_loading.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        genreFragment_loading.visibility = View.VISIBLE
     }
 
 }
