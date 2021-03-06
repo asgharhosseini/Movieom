d81ecffda3e84721acbacece4a5093474b21e721
asghar hosseini <asgharhosseini.v@gmail.com>
2021-03-06 11:46:48 +0330

    The change in the dependency of the movie adapter was a problem in the Singleton type, which caused problems with the multiplicity of builds

--- app/src/main/java/ir/vbile/app/movieom/di/AppModule.kt
+++ app/src/main/java/ir/vbile/app/movieom/di/AppModule.kt
@@ -5,22 +5,20 @@ import com.bumptech.glide.*
 import com.bumptech.glide.load.engine.*
 import com.bumptech.glide.load.resource.bitmap.*
 import com.bumptech.glide.request.*
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
+import com.google.gson.*
+import dagger.*
+import dagger.hilt.*
 import dagger.hilt.android.qualifiers.*
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.components.*
 import ir.vbile.app.movieom.R
-import ir.vbile.app.movieom.data.network.ApiService
-import ir.vbile.app.movieom.data.network.EndPoint
+import ir.vbile.app.movieom.data.model.movies.*
+import ir.vbile.app.movieom.data.network.*
 import ir.vbile.app.movieom.data.repositories.*
+import ir.vbile.app.movieom.other.*
 import ir.vbile.app.movieom.ui.adapter.*
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
+import retrofit2.*
+import retrofit2.converter.gson.*
+import javax.inject.*
 
 
 @Module
@@ -53,14 +51,17 @@ object AppModule {
     @Singleton
     @Provides
     fun provideApiService(gson: Gson): ApiService =
-        Retrofit.Builder()
-            .baseUrl(EndPoint.API_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-            .create(ApiService::class.java)
+            Retrofit.Builder()
+                    .baseUrl(EndPoint.API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build()
+                    .create(ApiService::class.java)
 
-    @Singleton
+    //    @Singleton
     @Provides
-    fun provideMovieAdapter(glide:RequestManager)= MoviesAdapter(glide)
+    fun provideMovieAdapter(glide: RequestManager) = MoviesAdapter(glide)
+
+
+
 
 }
