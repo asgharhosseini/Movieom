5b62079f14b715c88c932587d8cdd937c2a3bc5d
asghar hosseini <asgharhosseini.v@gmail.com>
2021-03-06 11:40:28 +0330

    Fixed the problem of receiving movies based on genre in Fragment Home and receiving a banner

--- app/src/main/java/ir/vbile/app/movieom/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/ir/vbile/app/movieom/ui/fragment/home/HomeViewModel.kt
@@ -14,18 +14,30 @@ import kotlin.random.*
 
 
 class HomeViewModel @ViewModelInject constructor(private val movieRepository: MovieRepository):ViewModel() {
-
+    var banner = Random.nextInt(1, 21)
+    var top = Random.nextInt(1, 21)
+    var center = Random.nextInt(1, 21)
+    var down = Random.nextInt(1, 21)
 
 
     private val _genre=MutableLiveData<Resource<List<Genre>>>()
     val genre: LiveData<Resource<List<Genre>>> =_genre
 
-    private val _genresMovies=MutableLiveData<Resource<MoviesResponse>>()
-    val genresMovies: LiveData<Resource<MoviesResponse>> =_genresMovies
+    private val _genresMoviesTop=MutableLiveData<Resource<MoviesResponse>>()
+    val genresMoviesTop: LiveData<Resource<MoviesResponse>> =_genresMoviesTop
+
+    private val _genresMoviesCenter=MutableLiveData<Resource<MoviesResponse>>()
+    val genresMoviesCenter: LiveData<Resource<MoviesResponse>> =_genresMoviesCenter
+
+    private val _genresMoviesDown=MutableLiveData<Resource<MoviesResponse>>()
+    val genresMoviesDown: LiveData<Resource<MoviesResponse>> =_genresMoviesDown
+
+    private val _genresMoviesBanner=MutableLiveData<Resource<MoviesResponse>>()
+    val genresMoviesBanner: LiveData<Resource<MoviesResponse>> =_genresMoviesBanner
 
     init {
         getGenres()
-
+        getGenresMovies(banner,top,center,down)
     }
 
 
@@ -46,16 +58,63 @@ class HomeViewModel @ViewModelInject constructor(private val movieRepository: Mo
     }
 
 
-    fun getGenresMovies(genreId: Int)=viewModelScope.launch {
-        _genresMovies.postValue(Resource.Loading())
+    fun getGenresMovies(genreIdBanner: Int,genreIdTop: Int,genreIdCenter: Int,genreIdDown: Int)=viewModelScope.launch {
+        _genresMoviesBanner.postValue(Resource.Loading())
+
+
         try {
-            val response = movieRepository.getGenresMovies(genreId)
-            handleResponse(response,_genresMovies)
+
+            val responseBanner = movieRepository.getGenresMovies(genreIdBanner)
+            handleResponse(responseBanner,_genresMoviesBanner)
 
         }catch (t:Throwable){
             when(t){
-                is IOException->_genresMovies.postValue(Resource.Error("Network Failure",null,404))
-                else->_genresMovies.postValue(Resource.Error("Conversion Error",null,404))
+                is IOException->_genresMoviesBanner.postValue(Resource.Error("Network Failure",null,404))
+                else->_genresMoviesBanner.postValue(Resource.Error("Conversion Error",null,404))
+            }
+
+        }
+        _genresMoviesTop.postValue(Resource.Loading())
+
+
+        try {
+
+            val responseTop = movieRepository.getGenresMovies(genreIdTop)
+            handleResponse(responseTop,_genresMoviesTop)
+
+        }catch (t:Throwable){
+            when(t){
+                is IOException->_genresMoviesTop.postValue(Resource.Error("Network Failure",null,404))
+                else->_genresMoviesTop.postValue(Resource.Error("Conversion Error",null,404))
+            }
+
+        }
+        _genresMoviesCenter.postValue(Resource.Loading())
+
+        try {
+
+            val responseCenter = movieRepository.getGenresMovies(genreIdCenter)
+            handleResponse(responseCenter,_genresMoviesCenter)
+
+
+        }catch (t:Throwable){
+            when(t){
+                is IOException->_genresMoviesCenter.postValue(Resource.Error("Network Failure",null,404))
+                else->_genresMoviesCenter.postValue(Resource.Error("Conversion Error",null,404))
+            }
+
+        }
+        _genresMoviesDown.postValue(Resource.Loading())
+        try {
+
+
+            val responseDown = movieRepository.getGenresMovies(genreIdDown)
+            handleResponse(responseDown,_genresMoviesDown)
+
+        }catch (t:Throwable){
+            when(t){
+                is IOException->_genresMoviesDown.postValue(Resource.Error("Network Failure",null,404))
+                else->_genresMoviesDown.postValue(Resource.Error("Conversion Error",null,404))
             }
 
         }
